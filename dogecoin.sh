3d921134c88e53bffeb835f01d71ae8ee633f087
nanakuryana41 <85402390+nanakuryana41@users.noreply.github.com>
2021-06-06 02:55:37 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u DOGE:DMTpDnNGLY2a3cavW8JBg8bgKiS8QBA81E.Pukiman -p x -k -a rx/0
